d3dc0512b849f6eab7667bf9bf60bd0a93e928a2
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:04:41 -0500

    Update StreamlitFrontend.py

--- StreamlitFrontend.py
+++ StreamlitFrontend.py
@@ -1,3 +1,6 @@
 import streamlit as st
+st.set_page_config(page_title="Scholarra", layout="wide")
 
+# Top bar
+col1, col2, col3, col4 = st.columns([1,2,2,1])  
 st.markdown('<h1 style="font-size:70px">Welcome, User.</h1>', unsafe_allow_html=True)
