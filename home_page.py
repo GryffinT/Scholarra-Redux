5d3e537e5980361022d0097e4377f3d370cd5ec2
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:37:59 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -4,13 +4,17 @@ def format_chat(message, size):
     st.markdown(f'<h1 style="font-size:{size}px">{message}</h1>', unsafe_allow_html=True)
 
 def display_home():
+    images_dir = os.path.join(base_dir, "media")
+    logo = [
+    os.path.join(images_dir, "excel_logo.png"),
+    ]
     st.markdown('<h1 style="font-size:70px; text-align:center;">Welcome, User.</h1>', unsafe_allow_html=True)
     st.markdown('<h1 style="font-size:70px; text-align:center;">Smarter study starts here</h1>', unsafe_allow_html=True)
     format_chat("Expand and feed your interests, understanding, and curiosities", 50)
     st.markdown('<h1 style="font-size:70px; text-align:center;">We offer Microsoft Excel prep materials.</h1>', unsafe_allow_html=True)
     
     st.markdown(
-    '<p style="text-align:center;"><img src="media/excel_logo.png" width="200"></p>',
+    f'<p style="text-align:center;"><img src="{logo[1]}" width="200"></p>',
     unsafe_allow_html=True)
 
 
