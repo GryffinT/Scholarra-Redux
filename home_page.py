ff9c8d9876d902925406fc92b09074943ed05cc0
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:49:06 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -13,7 +13,11 @@ def display_home():
     st.title("")
     st.title("")
     st.title("")
-    st.markdown('<h1 style="font-size:70px; text-align:center;">Smarter study starts here</h1>', unsafe_allow_html=True)
+    st.markdown("""
+        <h1 style="font-size:70px; text-align:center; font-family:'Josefin Sans', sans-serif;">
+            Smarter study starts here
+        </h1>
+    """, unsafe_allow_html=True)
     format_chat("Expand and feed your interests, understanding, and curiosities", 50)
     st.markdown('<h1 style="font-size:70px; text-align:center;">We offer Microsoft Excel prep materials.</h1>', unsafe_allow_html=True)
     
