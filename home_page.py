e7fe14d3554814be6180b97977a04ce1b3de5988
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:15:23 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def format_chat(message, size):
+  st.markdown(f'<h1 style="font-size:{size}px">{message}</h1>', unsafe_allow_html=True)
+
+
+st.markdown('<h1 style="font-size:70px">Welcome, User.</h1>', unsafe_allow_html=True)
+
+st.markdown('<h1 style="font-size:70px">Samrter study starts here</h1>', unsafe_allow_html=True)
+format_chat("Expand and feed your interests, understanding, and curiosities", 50)
 
