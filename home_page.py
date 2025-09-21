9cfee3d7761cdb3079e9d8bcb3f30e010631325a
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 18:02:19 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -73,7 +73,7 @@ def embed(message, size, centering, extra=None):
 
 
 def display_home():
-    graphic(1, 20)
+    graphic(1, 1)
     format_chat("Smarter study starts here.", 25, 1)
     pad(2)
     format_chat("Expand and feed your interests, understanding, and curiosities", 50, 1)
