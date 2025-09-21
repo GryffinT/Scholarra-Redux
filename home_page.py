de9290317700103e762bb7780726931f7461bc8c
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 18:46:04 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -10,7 +10,7 @@ logo = [
 
 def contain(*messages):
     # Start the div with styles
-    html_content = '<div style="border:50px #d4d4d4; background-color:#FFFFF; padding:10px; border-radius:5px;">'
+    html_content = '<div style="border:50px #d4d4d5; background-color:#d4d4d5; padding:10px; border-radius:5px;">'
     
     # Add each message to the div
     for msg, size, align in messages:
