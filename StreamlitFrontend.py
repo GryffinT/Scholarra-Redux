a3f88afc708404d1ba218e1fb5ba597e6012a760
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:05:07 -0500

    Update StreamlitFrontend.py

--- StreamlitFrontend.py
+++ StreamlitFrontend.py
@@ -3,4 +3,10 @@ st.set_page_config(page_title="Scholarra", layout="wide")
 
 # Top bar
 col1, col2, col3, col4 = st.columns([1,2,2,1])  
+
+
+with col2:
+    st.markdown("### 🏠 Home")
+with col3:
+    st.markdown("### 📚 Courses")
 st.markdown('<h1 style="font-size:70px">Welcome, User.</h1>', unsafe_allow_html=True)
