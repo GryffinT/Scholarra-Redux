5dcfe506fc3afc35bf246c1618887b2a7186b8a6
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 17:16:13 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -48,14 +48,14 @@ def display_home():
     tab1, tab2, tab3 = st.tabs(["A site redux", "Meet Laurent", "Excel, with Scholarra"])
     
     with tab1:
-        format_chat("A site redux!", 25, 0)
-        format_chat("We did an entire site-wide redux to improve UI experience and backend work!", 12, 0)
+        format_chat("A site redux!", 40, 0)
+        format_chat("We did an entire site-wide redux to improve UI experience and backend work!", 25, 0)
     with tab2:
-        format_chat("Meet Laurent", 25, 1)
-        format_chat("The newest member to the Scholarra team! Laurent.FP16, a Logistic Regression Transformer with Float Point 16 precision, can be found on the chat tab!", 12, 1)
+        format_chat("Meet Laurent", 40, 1)
+        format_chat("The newest member to the Scholarra team! Laurent.FP16, a Logistic Regression Transformer with Float Point 16 precision, can be found on the chat tab!", 25, 1)
     with tab3:
-        format_chat("Excel, with Scholarra!", 25, 2)
-        format_chat("We're pleased to announce that starting now, 9/23/2025, users can now access the Excel prep course through the course tab, free of cost!", 12, 2)
+        format_chat("Excel, with Scholarra!", 40, 2)
+        format_chat("We're pleased to announce that starting now, 9/23/2025, users can now access the Excel prep course through the course tab, free of cost!", 25, 2)
 
 
 
