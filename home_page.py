7335b801a5d79d98ceea4deea8d20427dae7ef69
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:55:29 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -2,24 +2,23 @@ import streamlit as st
 import os
 
 def format_chat(message, size):
-    st.markdown(f'<h1 style="font-size:{size}px">{message}</h1>', unsafe_allow_html=True)
-
+    st.markdown(f"""
+        <h1 style="font-size:{size}px; text-align:center; font-family:'Josefin Sans', sans-serif;">
+            {message}
+        </h1>
+    """, unsafe_allow_html=True)
 def display_home():
     images_dir = os.path.join(os.path.dirname(__file__), "media")
     logo = [
     os.path.join(images_dir, "excel_logo.png"),
     ]
-    st.markdown('<h1 style="font-size:70px; text-align:center;">Welcome, User.</h1>', unsafe_allow_html=True)
+    format_chat("Welcome, User.", 70)
     st.title("")
     st.title("")
     st.title("")
-    st.markdown("""
-        <h1 style="font-size:70px; text-align:center; font-family:'Josefin Sans', sans-serif;">
-            Smarter study starts here
-        </h1>
-    """, unsafe_allow_html=True)
+    format_chat("Smarter study starts here.", 50)
     format_chat("Expand and feed your interests, understanding, and curiosities", 50)
-    st.markdown('<h1 style="font-size:70px; text-align:center;">We offer Microsoft Excel prep materials.</h1>', unsafe_allow_html=True)
+    format_chat("We offer Microsoft Excel prep materials.", 70)
     
     st.markdown(
     f'<p style="text-align:center;"><img src="{logo[0]}" width="200"></p>',
