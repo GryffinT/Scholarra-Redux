3dec2adf810ffdbb79ab6f72dbb25275f82a1897
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 16:59:43 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import os
 
-def format_chat(message, size):
-    st.markdown(f"""
-        <h1 style="font-size:{size}px; text-align:center; font-family:'Josefin Sans', sans-serif;">
-            {message}
-        </h1>
-    """, unsafe_allow_html=True)
+
+def format_chat(message, size, centering):
+    mid = "center"
+    if centered == True:
+        st.markdown(f"""
+            <h1 style="font-size:{size}px; text-align:{centering}; font-family:'Josefin Sans', sans-serif;">
+                {message}
+            </h1>
+        """, unsafe_allow_html=True)
 def display_home():
     images_dir = os.path.join(os.path.dirname(__file__), "media")
     logo = [
@@ -16,9 +19,9 @@ def display_home():
     st.title("")
     st.title("")
     st.title("")
-    format_chat("Smarter study starts here.", 50)
-    format_chat("Expand and feed your interests, understanding, and curiosities", 50)
-    format_chat("We offer Microsoft Excel prep materials.", 70)
+    format_chat("Smarter study starts here.", 50, mid)
+    format_chat("Expand and feed your interests, understanding, and curiosities", 50, mid)
+    format_chat("We offer Microsoft Excel prep materials.", 70, mid)
     
     st.markdown(
     f'<p style="text-align:center;"><img src="{logo[0]}" width="200"></p>',
