3724ab2b483fa318e3858151ea32882fd0942fbd
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 19:08:27 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -98,7 +98,7 @@ def embed(message, size, centering, extra=None):
 
 
 def display_home():
-    st.image(logo[2])
+    st.image(logo[2], use_column_width=True)
     pad(2)
     graphic(1, 600)
     format_chat("Smarter study starts here.", 25, 1)
