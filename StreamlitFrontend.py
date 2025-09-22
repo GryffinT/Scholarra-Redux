1b42caf0a69587d768b9a49b9b46920812272eff
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 22:29:26 -0500

    Update StreamlitFrontend.py

--- StreamlitFrontend.py
+++ StreamlitFrontend.py
@@ -49,17 +49,17 @@ with tab1:
     st.session_state.page = 1
     if st.session_state.page == 1:
         display_home()
-        st_autorefresh(interval=840000, key="auto_refresh")
+        st_autorefresh(interval=840000, key="1_auto_refresh")
 
 with tab2:
     st.session_state.page = 2
     if st.session_state.page == 2:
         display_course()
-        st_autorefresh(interval=840000, key="auto_refresh")
+        st_autorefresh(interval=840000, key="2_auto_refresh")
 
 with tab3:
     st.session_state.page = 3
     if st.session_state.page == 3:
         st.header("An owl")
         st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
-        st_autorefresh(interval=840000, key="auto_refresh")
+        st_autorefresh(interval=840000, key="3_auto_refresh")
