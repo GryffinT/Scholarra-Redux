ca8ce75964b1c3c1623149a50a8954e07289d186
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 23:55:45 -0500

    Update course_page.py

--- course_page.py
+++ course_page.py
@@ -7,8 +7,8 @@ medias = [
     os.path.join(images_dir, "excel_medias.png"),
     os.path.join(images_dir, "scholarracourselogo.png"),
     os.path.join(images_dir, "courseimage.jpg"),
-    os.path.join(images_dir, "Syllabus TTS.mp3")
-    os.path.join(images_dir, "mo-200-microsoft-excel-2019-skills.measured (2).pdf)
+    os.path.join(images_dir, "Syllabus TTS.mp3"),
+    os.path.join(images_dir, "mo-200-microsoft-excel-2019-skills.measured (2).pdf")
     
 ]
 
