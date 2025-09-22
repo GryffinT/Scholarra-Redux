a9ce96440b943d9e3ad236dd5b7df2fcf26164d4
Gryffin T <Gryffintubon@gmail.com>
2025-09-21 20:20:15 -0500

    Update home_page.py

--- home_page.py
+++ home_page.py
@@ -5,7 +5,7 @@ import base64
 images_dir = os.path.join(os.path.dirname(__file__), "media")
 logo = [
     os.path.join(images_dir, "excel_logo.png"),
-    os.path.join(images_dir, "Scholarra Splotch Logo.png"),
+    os.path.join(images_dir, "splotchLogo.png"),
     os.path.join(images_dir, "Library.jpg")
 ]
 
