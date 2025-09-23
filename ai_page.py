76ad403fb5e39c5eef421e327c286a746a33bf16
Gryffin T <Gryffintubon@gmail.com>
2025-09-22 22:33:28 -0500

    Update ai_page.py

--- ai_page.py
+++ ai_page.py
@@ -34,8 +34,7 @@ def display_ai():
     # Chat panel
     # -------------------------
     
-    contain(("Welcome, User", 40, 1), ("What\'s on today\'s agenda?", 25, 1))
-    st.markdown('<h1 style="font-size:30px">What\'s on today\'s agenda?</h1>', unsafe_allow_html=True)
+    contain(("Welcome, User", 50, 1), ("What\'s on today\'s agenda?", 25, 1))
     # Initialize chat history
     if "messages" not in st.session_state:
         st.session_state.messages = []
